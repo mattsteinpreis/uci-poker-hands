8d428f40b478b17d92d66cde6d6bc787f476a4b8
mattsteinpreis <matt@nexosis.com>
2015-11-13 17:02:23 -0500

    module name change

--- scripts/01-download-data.py
+++ scripts/01-download-data.py
@@ -4,9 +4,9 @@ import os.path
 __author__ = 'matt'
 __project__ = 'uci-poker-hands'
 
-funcs = imp.load_source('funcs', '../lib/poker.py')
+poker = imp.load_source('poker', '../lib/poker.py')
 
 if not os.path.isfile("../data/poker-hand-training-true.data"):
-    funcs.download_training_data()
+    poker.download_training_data()
 if not os.path.isfile("../data/poker-hand-testing.data"):
-    funcs.download_test_data()
+    poker.download_test_data()
